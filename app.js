479bde8017cf819073844f111cdf6f1c3f466225
benleahy91 <benleahy91@gmail.com>
2019-02-07 17:22:52 -0500

    uploading images

--- app.js
+++ app.js
@@ -3,12 +3,38 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
+const fileStorage = multer.diskStorage({
+	destination: (req, file, cb) => {
+		cb(null, 'images');
+	},
+	filename: (req, file, cb) => {
+		cb(null, new Date().toISOString() + '-' + file.originalname);
+	}
+});
+
+const fileFilter = (req, file, cb) => {
+	if (
+		file.mimetype === 'image/png' ||
+		file.mimetype === 'image/jpg' ||
+		file.mimetype === 'image/jpeg'
+	) {
+		cb(null, true);
+	} else {
+		cb(null, false);
+	}
+} 
+
 app.use(bodyParser.json());
+app.use(
+  multer({ storage: fileStorage, fileFilter: fileFilter }).single('image')
+);
+
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
@@ -31,10 +57,10 @@ app.use((error, req, res, next) => {
 });
 
 mongoose
-.connect(
-	'mongodb://nodeben:nodeBen34@ds145704.mlab.com:45704/nodeshop'
-)
-.then(result => {
-	app.listen(8080);
-})
-.catch(err => console.log(err));
+	.connect(
+		'mongodb://nodeben:nodeBen34@ds145704.mlab.com:45704/nodeshop'
+	)
+	.then(result => {
+		app.listen(8080);
+	})
+	.catch(err => console.log(err));
--- controllers/feed.js
+++ controllers/feed.js
@@ -3,20 +3,19 @@ const { validationResult } = require('express-validator/check');
 const Post = require('../models/post');
 
 exports.getPosts = (req, res, next) => {
-  res.status(200).json({
-    posts: [
-      {
-        _id: '1',
-        title: 'First Post',
-        content: 'This is the first post!',
-        imageUrl: 'images/tomato.jpg',
-        creator: {
-          name: 'Ben'
-        },
-        createdAt: new Date()
-      }
-    ]
-  });
+	Post
+	.find()
+	.then(posts => {
+		res
+		.status(200)
+		.json({message: 'Fetched posts successfuly.', posts: posts})
+	})
+	.catch(err => {
+		if(!err.statusCode) {
+			err.statusCode = 500;
+		};
+		next(err);
+	});
 };
 
 exports.createPost = (req, res, next) => {	
@@ -26,12 +25,18 @@ exports.createPost = (req, res, next) => {
 		error.statusCode = 422;
 		throw error;		
 	};
+	if (!req.file) {
+		const error = new Error('No image provided');
+		error.statusCode = 422;
+		throw error;
+	};
+	const imageUrl = req.file.path;
   const title = req.body.title;
   const content = req.body.content;
   const post = new Post({
     title: title,
     content: content,
-    imageUrl: 'images/tomato.jpg',
+    imageUrl: imageUrl,
     creator: { name: 'Benjamin' }
   });
   post
@@ -41,6 +46,25 @@ exports.createPost = (req, res, next) => {
 			message: 'Post created successfully!',
 			post: result
 		});
+	})
+	.catch(err => {
+		if(!err.statusCode) {
+			err.statusCode = 500;
+		};
+		next(err);
+	});
+};
+
+exports.getPost = (req, res, next) => {
+	const postId = req.params.postId;
+	Post.findById(postId)
+	.then(post => {
+		if(!post) {
+			const error = new Error('Could not find post.')
+			error.statusCode = 404;
+			throw error;
+		};
+		res.status(200).json({message: 'Post fetched.', post: post});
 	})
 	.catch(err => {
 		if(!err.statusCode) {
