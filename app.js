793c2c569dedb3830f535c408df71939f5e46efb
benleahy91 <benleahy91@gmail.com>
2019-02-07 11:32:57 -0500

    error handling

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 
 const feedRoutes = require('./routes/feed');
 
 const app = express();
 
 app.use(bodyParser.json());
+app.use('/images', express.static(path.join(__dirname, 'images')));
 
 app.use((req, res, next) => {
-	res.setHeader('Access-Control-Allow-Origin', '*');
-	res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, PATCH, DELETE');
-	res.setHeader('Access-Control-Alllow-Headers', 'Content-Type, Authorization');
-	next();
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'OPTIONS, GET, POST, PUT, PATCH, DELETE'
+  );
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
 });
 
 app.use('/feed', feedRoutes);
 
-app.listen(8080);
+app.use((error, req, res, next) => {
+	console.log(error);
+	const status = error.statusCode || 500;
+	const message = error.message;
+	res.status(status).json({message: message});
+});
+
+mongoose
+.connect(
+	'mongodb://nodeben:nodeBen34@ds145704.mlab.com:45704/nodeshop'
+)
+.then(result => {
+	app.listen(8080);
+})
+.catch(err => console.log(err));
